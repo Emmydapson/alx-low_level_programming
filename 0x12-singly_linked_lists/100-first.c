2a9f411f37de2c1f63b59e94a505f43813eab07c
emmydapson <iloriemmanuel00@gmail.com>
2023-08-25 22:51:09 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * before_main - Function executed before main
+ *
+ * Description: This function is executed before the main function and
+ *              prints two lines of text.
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("This is the main function.\n");
+	return (0);
+}
+
